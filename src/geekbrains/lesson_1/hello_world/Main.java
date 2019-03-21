f59d7c0de351eddede4f402e358ea5c9e60443d9
NiCOOLaS55 <pkrnl2012@gmail.com>
2019-03-21 18:07:35 +0300

    угадать число

--- src/geekbrains/lesson_1/hello_world/Main.java
+++ src/geekbrains/lesson_1/hello_world/Main.java
@@ -1,8 +1,27 @@
 package geekbrains.lesson_1.hello_world;
 
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ваша задача угадать число.");
+        int range = 100;
+        int number = (int)(Math.random()*range);
+        while (true) {
+            System.out.println("Угадайте число от 0 до " + range);
+            int input_number = scanner.nextInt();
+            if (input_number == number) {
+                System.out.println("Вы угадали");
+                break;
+            } else if (input_number > number) {
+                System.out.println("Загаданное число меньше");
+            } else {
+                System.out.println("Загаданное число больше");
+            }
+        }
+        scanner.close();
     }
 }
